6f7126e235857b280b4af4e207381551534dce64
Zach Miller <zach@zarfmouse.us>
2012-04-18 20:06:58 -0500

    track complete but unresolved tasks and add data gathering fields

--- web/script.js
+++ web/script.js
@@ -40,28 +40,111 @@ $.extend(EventReader.prototype, {
     }
 });
 
-var events = new EventReader('task-status.php');
-events.setEventHandler(function(data) { 
-    var saw = {};
-    var sorted_keys = Object.keys(data).sort();
-    $.each(sorted_keys, function(index, key) {
-	var value = data[key];
-	var id = key.replace(/[^a-z0-9A-Z]/g, '_');
-	var e = $('#'+id);
-	if(e.length == 0) {
-	    $("#tasks").append('<li class="task active" id="'+id+'"><div class="cell key">'+key+'</div><div class="cell type"></div><div class="cell meta"></div><div class="cell progress_container"><div class="message"></div><div class="progress"></div></div></li>');
-	    e = $('#'+id);
+var RipAudio = function(id) {this.init(id);};
+$.extend(RipAudio.prototype, {
+    element: undefined,
+    init: function(id) {
+	this.element = $('<li class="task"><div class="cell key">'+id+'</div><div class="cell type">RipAudio</div><div class="cell meta"></div><div class="cell progress_container"><div class="message"></div><div class="progress"></div></div></li>');
+	this.addBarCode();
+	this.addCDDB();
+	this.addMusicBrainz();
+    },
+    getId: function() {
+	return this.element.find('.key').html();
+    },
+    setActive: function(active) {
+	if(active) {
+	    this.element.addClass('active');
+	} else {
+	    this.element.removeClass('active');
+	    this.setMessage("Complete");
 	}
-	saw[id] = true;
-	$('#'+id+' .message').html(value.message);
-	$('#'+id+' .type').html(value.type);
-	$('#'+id+' .progress').progressbar({value: value.percent});
-    });
-    $(".task.active").each(function(i,e) {
-	var id = $(e).attr('id');
-	if(saw[id] !== true) {
-	    $(e).removeClass('active');
-	}
-    });
+    },
+    setMessage: function(message) {
+	this.element.find('.message').html(message);
+    },
+    setProgress: function(percent) {
+	this.element.find('.progress').progressbar({value: percent});
+    },
+    insert: function(root) {
+	$(root).append(this.element);
+    },
+    appendMeta: function(meta) {
+	this.element.find('.meta').append(meta);
+    },
+    addBarCode: function() {
+	var wrapper = $('<div class="barcode"></div>');
+	var input = $('<input type="text" name="barcode" value=""/>');
+	wrapper.append(input);
+	this.appendMeta(wrapper);
+	var obj = this;
+	input.change(function (ev) {
+	    console.log('changed barcode on '+obj.getId()+' to '+$(this).val());
+	});
+    },
+    addCDDB: function() {
+	var wrapper = $('<div class="cddb"></div>');
+	var select = $('<select name="cddb"><option>Loading...</option><option></option></select>');
+	wrapper.append(select);
+	this.appendMeta(wrapper);
+	var obj = this;
+	select.change(function (ev) {
+	    console.log('changed cddb on '+obj.getId()+' to '+$(this).val());
+	});
+    },
+    addMusicBrainz: function() {
+	var wrapper = $('<div class="musicbrainz"></div>');
+	var select = $('<select name="musicbrainz"><option>Loading...</option><option></option></select>');
+	wrapper.append(select);
+	this.appendMeta(wrapper);
+	var obj = this;
+	select.change(function (ev) {
+	    console.log('changed musicbrainz on '+obj.getId()+' to '+$(this).val());
+	});
+    },
 });
-events.run();
+
+$.extend(RipAudio, {
+    cache: {},
+    root: '#tasks',
+    find: function(id) {
+	var retval = this.cache[id];
+	return retval;
+    },
+    find_or_create: function(id) { 
+	var retval = this.find(id);
+	if(retval === undefined) {
+	    retval = new this(id);
+	    retval.insert(this.root);
+	    this.cache[id] = retval;
+	} 
+	return this.find(id);
+    },
+    update_from_events: function(data) {
+	var saw = {};
+	var sorted_keys = Object.keys(data).sort();
+	var obj = this;
+	$.each(sorted_keys, function(index, key) {
+	    var value = data[key];
+	    // TODO: Refactor to allow multiple types using value.type.
+	    var task = obj.find_or_create(key);
+ 	    saw[key] = true;
+	    task.setProgress(value.percent);
+	    task.setMessage(value.message);
+	    task.setActive(value.message != 'Complete');
+	});
+	$(".task.active").each(function(i,e) {
+	    var id = $(e).find('.key').html();
+	    var task = obj.find(id);
+	    $(e).setActive(saw[id] === true);
+	});
+    },
+});
+
+$.getJSON('unresolved-tasks.php', function(data) {
+    RipAudio.update_from_events(data);
+    var events = new EventReader('task-status.php');
+    events.setEventHandler(RipAudio.update_from_events); 
+    events.run();
+});
+
---
+++ web/unresolved-tasks.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__."/../lib/autoload-init.php";
+require_once __DIR__."/../lib/doctrine-init.php";
+
+use Doctrine\Common\Persistence\PersistentObject;
+
+$entityManager = PersistentObject::getObjectManager();
+
+$tasks = $entityManager->getRepository('ZRipEntities\RipAudio')->findBy(['resolved' => false, 'complete' => true]);
+$output = [];
+foreach($tasks as $task) {
+  $output[$task->getUuid()] = ['percent' => 100,
+			       'message' => 'Complete',
+			       'type' => 'RipAudio'];
+}
+header("Content-type: application/json");
+print json_encode($output);
+
